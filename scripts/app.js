ee87650538a9f94179038ace92fc2b8222d984cb
Phil Barbato <phil.barbato@teamcolab.com>
2019-01-02 01:26:25 -0500

    new sites added, need descriptions.

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,10 @@
 // Import the init functions
 // Default export: import ANY_NAME_YOU_WANT from 'folder/module'
 // Named export: import { FUNCTION as ANY_NAME } from 'folder/module'
-// import mobileNavigation from './modules/mobile-navigation';
+import taglines from './modules/taglines';
 
 // Vanilla JS version of $(document).ready() works in IE9+
-// document.addEventListener('DOMContentLoaded', function () {
-//     mobileNavigation();
-// });
+document.addEventListener('DOMContentLoaded', function () {
+    taglines();
+});
 
---
+++ scripts/modules/taglines.js
@@ -0,0 +1,16 @@
+const taglines = [
+    'I write the SASS that makes the whole world sing.',
+    'I love to listen. I love to teach.'
+]
+
+const s = {
+    heading: document.querySelector('.content__heading')
+}
+
+function init() {
+    // bind all Module Actions
+    var randomIndex = Math.floor(Math.random() * taglines.length);
+    s.heading.innerHTML = taglines[randomIndex];
+}
+
+export { init as default }
