a1222cd0a1fd7254389e0e362312acea6a2e08bf
Phil Barbato <phil.barbato@teamcolab.com>
2019-01-03 08:28:42 -0500

    tweak to headline. Adding some more headlines to the big list.

--- scripts/modules/taglines.js
+++ scripts/modules/taglines.js
@@ -1,7 +1,13 @@
 const taglines = [
     'I write the SASS that makes the whole world sing.',
     'I love to listen. I love to teach.',
-    'I specialize in metaphor.'
+    'I specialize in metaphor.',
+    'Dear God! What is that thing?',
+    'Fresh internet since 1999.',
+    'Provider of <em>je ne sais quoi</em> for over 20 years.',
+    'I&rsquo;ve got a fever, and the only prescription is more <em>user stories</em>.',
+    'Brings out the flavor.'
+
 ]
 
 const s = {
